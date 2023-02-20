2423b405487090961a59e0b5b2731f6d3b22cc8c
Xiao Zhang <xxxiao971006@gmail.com>
2023-02-19 17:40:12 -0800

    second commit

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -19,5 +19,57 @@ async function getAllUsers() {
 	}
 }
 
+async function addUser(postData) {
+	let sqlInsertSalt = `
+INSERT INTO web_user (first_name, last_name, email, password_salt)
+VALUES (:first_name, :last_name, :email, sha2(UUID(),512));
+`;
+	let params = {
+		first_name: postData.first_name,
+		last_name: postData.last_name,
+		email: postData.email
+	};
+	console.log(sqlInsertSalt);
+	try {
+		const results = await database.query(sqlInsertSalt, params);
+		let insertedID = results.insertId;
+		let updatePasswordHash = `
+UPDATE web_user
+SET password_hash = sha2(concat(:password,:pepper,password_salt),512)
+WHERE web_user_id = :userId;
+`;
+		let params2 = {
+			password: postData.password,
+			pepper: passwordPepper,
+			userId: insertedID
+		}
+		console.log(updatePasswordHash);
+		const results2 = await database.query(updatePasswordHash, params2);
+		return true;
+	}
+	catch (err) {
+		console.log(err);
+		return false;
+	}
+}
 
-module.exports = {getAllUsers}
+async function deleteUser(webUserId) {
+	let sqlDeleteUser = `
+DELETE FROM web_user
+WHERE web_user_id = :userID
+`;
+	let params = {
+		userID: webUserId
+	};
+	console.log(sqlDeleteUser);
+	try {
+		await database.query(sqlDeleteUser, params);
+		return true;
+	}
+	catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+module.exports = { getAllUsers, addUser, deleteUser }
--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,5 @@
+const { addUser } = require('../databaseAccessLayer');
+
 const router = require('express').Router();
 const database = include('databaseConnection');
 const dbModel = include('databaseAccessLayer');
@@ -19,9 +21,44 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.post('/addUser', (req, res) => {
+router.post('/addUser', async (req, res) => {
 	console.log("form submit");
 	console.log(req.body);
+
+	try {
+		const success = await dbModel.addUser(req.body);
+		if (success) {
+			res.redirect("/");
+		}
+		else {
+			res.render('error', { message: "Error writing to MySQL" });
+			console.log("Error writing to MySQL");
+		}
+	}
+	catch (err) {
+		res.render('error', { message: "Error writing to MySQL" });
+		console.log("Error writing to MySQL");
+		console.log(err);
+	}
+
 });
+
+router.get('/deleteUser', async (req, res) => {
+	console.log("delete user");
+	console.log(req.query);
+	let userId = req.query.id;
+	if (userId) {
+		const success = await dbModel.deleteUser(userId);
+		if (success) {
+			res.redirect("/");
+		}
+		else {
+			res.render('error', { message: 'Error writing to MySQL' });
+			console.log("Error writing to mysql");
+			console.log(err);
+		}
+	}
+});
+
 
 module.exports = router;
