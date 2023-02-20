7bac85d8ec171d39ec15fb20b0dd150d241678af
Xiao Zhang <xxxiao971006@gmail.com>
2023-02-19 17:17:45 -0800

    first commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -7,7 +7,8 @@ const dbConfigQoddi = {
 	user: "freedb_ 2350_main.",
 	password: "Dr*RW6#5b%&u6Cf",
 	database: "freedb_comp2350-week2-A01325043",
-	multipleStatements: false
+	multipleStatements: false,
+	namePlaceholders: true
 };
 
 const dbConfigLocal = {
@@ -15,7 +16,9 @@ const dbConfigLocal = {
 	user: "root",
 	password: "Password",
 	database: "database1",
-	multipleStatements: false
+	multipleStatements: false,
+	namePlaceholders: true
+
 };
 
 if (is_qoddi) {
